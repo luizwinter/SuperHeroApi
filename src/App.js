c9f20efd59b2e0ecc0f556ec22756f7d397ea386
Luizwinter <junior.winter@gmail.com>
2021-08-19 12:27:47 -0300

    Adicionando novos Cards.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Card from './components/Card';
 import styled from 'styled-components';
@@ -8,49 +8,117 @@ import Footer from './components/Footer';
 
 const baseURL = 'https://www.superheroapi.com/api.php/10226445205696427/'
 
-var count = 1;
+var count = [1, 2, 1, false];
+var canchange = true;
+
 
 export default function App(){
 
-  const [hero, setHero] = React.useState(null);
 
-  React.useEffect(()=>{
-    axios.get(`${baseURL}${count}`).then((response) =>{
-      setHero(response.data);
-    });
-  }, [nextHero]);
+
+  const [hero, setHero] = useState(null);
+  const [previouscard, setPreviousCard] = useState(null)
+  const [nextcard, setNextCard] = useState(null)
+
+  
+
+  useEffect(()=>{
+
+    checkChange()
+
+  }, []);
+
+  function checkChange(){
+    console.log("chanchenge?");
+
+    if(canchange === true) {
+
+      console.log("yes")
+
+      console.log("enter effect")
+
+
+      axios.get(`${baseURL}${count[0]}`).then((response) =>{
+        setHero(response.data);
+  
+      });
+      axios.get(`${baseURL}${count[1]}`).then((response) =>{
+        setNextCard(response.data);
+  
+      });
+
+      axios.get(`${baseURL}${count[2]}`).then((response) =>{
+        setPreviousCard(response.data);
+  
+      });
+
+        canchange = false;
+    }
+  }
+
 
     if(!hero) return null;    
+    if(!nextcard) return null;    
+    if(!previouscard) return null;    
+
+
     function nextHero(){
-      count += 1;      
+
+      if(count[3] === false){
+        count[0] += 1;  
+        count[1] += 1;  
+
+        count[3] = true;
+      }if(count[1] === 732){
+        count[0] += 1;  
+        count[2] += 1;  
+      } else{
+        count[0] += 1;  
+        count[1] += 1;  
+        count[2] += 1;  
+      };
+
+      canchange = true;
+
+      console.log(count)  
+        
+      checkChange()
     };
 
     function previousHero(){
-      if(count - 1 === 0){
-        count= 1;
+      if(count[0] - 1 === 0 | count[0] -1 === 1){
+        count[0]= 1;
+        count[2]= 1;
+
+        count[3] = false;
       }else{
-        count += -1;
+        count[0] += -1;
+        count[1] += -1;
+        count[2] += -1;
+
       };
+
+      canchange = true;
+      checkChange()
     };
 
     function randomHero(){
       let x = Math.floor(Math.random() * 732);
-      count = x;
+      count[0] = x;
+      count[1] = x+1;
+      count[2] = x-1;
 
+      canchange = true;
+      checkChange()
     }
-
-    
-
-
-
-
-
     return(
       <>
       <Box>
         <Banner />
         <Container>
           <Card img={hero.image.url}
+                card1={nextcard.image.url}
+                card2={previouscard.image.url}
             name={hero.name}
             bio={hero.work.occupation}
             int={hero.powerstats.intelligence} 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,7 +4,15 @@ const Card = (props)=>{
     return(
         <>
         <Wrap>
+        <FrontCard id="frontcard">
         <Img src={props.img} alt="Super Hero Img"/>
+        </FrontCard>
+        <BackCard>
+            <Img src={props.card2} alt="NextSuper Hero Img"/>
+            <Img src={props.card1} alt="NextSuper Hero Img"/>       
+        </BackCard>
+
+
         <H2>{props.name}</H2>
         <p>{props.bio}</p>
             <div>
@@ -59,11 +67,13 @@ const Wrap = styled.div`
 `;
 
 const Img = styled.img`
-    height: 300px;
+&{    height: 300px;
     object-fit: cover;
     border-radius: 5%;
-    -webkit-box-shadow: 5px 5px 15px 5px #FF8080, -9px 5px 15px 5px #FFE488, -7px -5px 15px 5px #8CFF85, 12px -5px 15px 5px #80C7FF, 12px 10px 15px 7px #E488FF, -10px 10px 15px 7px #FF616B, -10px -7px 27px 1px #8E5CFF, 5px 5px 15px 5px rgba(0,0,0,0); 
-box-shadow: 5px 5px 15px 5px #FF8080, -9px 5px 15px 5px #FFE488, -7px -5px 15px 5px #8CFF85, 12px -5px 15px 5px #80C7FF, 12px 10px 15px 7px #E488FF, -10px 10px 15px 7px #FF616B, -10px -7px 27px 1px #8E5CFF, 5px 5px 15px 5px rgba(0,0,0,0);
+    z-index: 1;
+    width: 250px;
+}
+
 `;
 
 const H2 = styled.h2`
@@ -71,6 +81,23 @@ const H2 = styled.h2`
     text-align: center;
     margin-bottom: 0;
 `
+
+const FrontCard = styled.div`
+    z-index: 2;
+    &#frontcard>IMG{ 
+    height: 350px;
+    z-index: 2;
+    transition: 0.3s;
+    -webkit-box-shadow: 5px 5px 15px 5px #FF8080, -9px 5px 15px 5px #FFE488, -7px -5px 15px 5px #8CFF85, 12px -5px 15px 5px #80C7FF, 12px 10px 15px 7px #E488FF, -10px 10px 15px 7px #FF616B, -10px -7px 27px 1px #8E5CFF, 16px 14px 26px 27px rgba(0,0,0,0); 
+box-shadow: 5px 5px 15px 5px #FF8080, -9px 5px 15px 5px #FFE488, -7px -5px 15px 5px #8CFF85, 12px -5px 15px 5px #80C7FF, 12px 10px 15px 7px #E488FF, -10px 10px 15px 7px #FF616B, -10px -7px 27px 1px #8E5CFF, 16px 14px 26px 27px rgba(0,0,0,0);
+  
+    }
+`;
+
+const BackCard = styled.div`
+    filter: grayscale(1);
+    position: absolute;
+`;
 
 
 export default Card;
